3c3ceee3c52ff1cc9857cec33b41a6e7f9bc9a1f
ARMAN-854 <singharmanpreet385@gmail.com>
2026-06-17 15:47:11 +0530

    Moved Checkout Page Into React.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import './HomePage.css';
 
 export function HomePage () {
     return(<>
+     <title>react-e_commerce</title>
          <div className="header">
       <div className="left-section">
         <a href="index.html" className="header-link">
