16c2867b2b552d55c54000ea1595faf02f20f80d
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-06-20 12:04:20 +0530

    Refactored Order Summary Into Checkout Folder.

---
+++ src/pages/checkout/CheckoutPage.jsx
@@ -0,0 +1,102 @@
+import axios from "axios";
+import { useState,useEffect } from "react";
+import { formatMoney } from "../../utils/money.js";
+import "./checkout-header.css";
+import "./CheckoutPage.css";
+import { OrderSummary } from "./OrderSummary.jsx";
+export function CheckoutPage({ cart }) {
+  const[deliveryOptions,setDeliveryOptions]=useState([])
+  const[paymentSummary,setPaymentSummary]=useState(null)
+  useEffect(()=>{
+    axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
+    .then((response)=>{
+      setDeliveryOptions(response.data)
+    });
+     axios.get('/api/payment-summary')
+    .then((response)=>{
+      setPaymentSummary(response.data)
+    });
+  },[])
+  return (
+    <>
+      <title>Checkout</title>
+      <div className="checkout-header">
+        <div className="header-content">
+          <div className="checkout-header-left-section">
+            <a href="/">
+              <img className="logo" src="images/logo.png" />
+              <img className="mobile-logo" src="images/mobile-logo.png" />
+            </a>
+          </div>
+
+          <div className="checkout-header-middle-section">
+            Checkout (
+            <a className="return-to-home-link" href="/">
+              3 items
+            </a>
+            )
+          </div>
+
+          <div className="checkout-header-right-section">
+            <img src="images/icons/checkout-lock-icon.png" />
+          </div>
+        </div>
+      </div>
+
+      <div className="checkout-page">
+        <div className="page-title">Review your orders</div>
+
+        <div className="checkout-grid">
+         <OrderSummary cart = {cart} deliveryOptions={deliveryOptions} />
+
+          <div className="payment-summary">
+            <div className="payment-summary-title">Payment Summary</div>
+{paymentSummary && (
+  <>
+     <div className="payment-summary-row">
+              <div>Items ({paymentSummary.totalItems}):</div>
+              <div className="payment-summary-money">
+                {formatMoney(paymentSummary.productCostCents)}
+              </div>
+            </div>
+
+            <div className="payment-summary-row">
+              <div>Shipping &amp; handling:</div>
+              <div className="payment-summary-money">
+                {formatMoney(paymentSummary.shippingCostCents)}
+              </div>
+            </div>
+
+            <div className="payment-summary-row subtotal-row">
+              <div>Total before tax:</div>
+              <div className="payment-summary-money">
+                {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
+              </div>
+            </div>
+
+            <div className="payment-summary-row">
+              <div>Estimated tax (10%):</div>
+              <div className="payment-summary-money">
+                {formatMoney(paymentSummary.taxCents)}
+              </div>
+            </div>
+
+            <div className="payment-summary-row total-row">
+              <div>Order total:</div>
+              <div className="payment-summary-money">
+                {formatMoney(paymentSummary.totalCostCents)}
+              </div>
+            </div>
+
+            <button className="place-order-button button-primary">
+              Place your order
+            </button>
+  </>
+)}
+         
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
