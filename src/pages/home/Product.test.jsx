d8e0c416b03c63974eb26da91e84f538e5fba58f
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-06-27 15:43:56 +0530

    Use beforeEach To Remove Duplication.

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,40 +1,51 @@
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { Product } from "./Product";
 import axios from "axios";
-vi.mock('axios')
+import { Product } from "./Product";
+
+vi.mock("axios");
+
+let product;
+let loadCart;
+
+beforeEach(() => {
+  vi.clearAllMocks();
+
+  axios.post.mockResolvedValue({
+    data: {},
+  });
+
+  product = {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: 1090,
+    keywords: ["socks", "sports", "apparel"],
+  };
+
+  loadCart = vi.fn();
+});
 
 describe("Product Component", () => {
   it("Displays Product Details Correctly", async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87,
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"],
-    };
-
-    const loadCart = vi.fn();
     const user = userEvent.setup();
 
     render(<Product product={product} loadCart={loadCart} />);
 
-    expect(
-      screen.getByText("Black and Gray Athletic Cotton Socks - 6 Pairs")
-    ).toBeInTheDocument();
-
     const addToCartButton = screen.getByTestId("add-to-cart-button");
 
     await user.click(addToCartButton);
-expect(axios.post).toHaveBeenCalledWith('api/cart-items',{
-    productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity : 1
-})
-    expect(loadCart).toHaveBeenCalled()
+
+    expect(axios.post).toHaveBeenCalledWith("api/cart-items", {
+      productId: product.id,
+      quantity: 1,
+    });
+
+    expect(loadCart).toHaveBeenCalled();
   });
 });
