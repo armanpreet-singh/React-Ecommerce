eb775c2f4f8d2c18f8dfc33d7511cd29059832a6
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-09 21:58:15 +0530

    Pass cart data to TrackingPage component.

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ loadCart();
       <Route index element = { <HomePage cart={cart} loadCart={loadCart} />}/>
       <Route path = "checkout" element = {<CheckoutPage cart={cart} loadCart = {loadCart} />} />
       <Route path = "orders" element = {<OrdersPage   cart={cart}/>} />
-      <Route path = "tracking" element = {<TrackingPage />} />
+      <Route path = "tracking" element = {<TrackingPage cart={cart} />} />
     </Routes>
   
   )
